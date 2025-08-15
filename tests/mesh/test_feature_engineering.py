a709e9322b8c97156ba57b8c6c5593b76ea233de
Fabien Casenave <fabien.casenave@gmail.com>
2025-08-15 18:28:48 +0200

    correct test

--- tests/mesh/test_feature_engineering.py
+++ tests/mesh/test_feature_engineering.py
@@ -5,5 +5,5 @@ class Test_Feature_Engineering:
     def test_compute_sdf(self, sample_with_tree):
         compute_sdf(sample_with_tree)
 
-    def update_sample_with_sdf(self, sample_with_tree):
+    def test_update_sample_with_sdf(self, sample_with_tree):
         update_sample_with_sdf(sample_with_tree)
