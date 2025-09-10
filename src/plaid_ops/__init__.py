168f89c1ec7672f99cc99a6d3708bcc628f83828
Fabien Casenave <fabien.casenave@gmail.com>
2025-09-10 19:58:48 +0200

    🎉 Add SDF and FE projections (#4)
    * add SDF computation function
    
    * disconnect pyright
    
    * fix(pyproject.toml) correct dependency
    
    * correct test
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * remopve plot from notebooks - not compiling online
    
    * update
    
    * update
    
    * add tests
    
    * update
    
    * up
    
    * add libosmesa6-dev for plot in actions
    
    * test
    
    * test
    
    * maj
    
    * maj
    
    * maj
    
    * update
    
    * update notebooks
    
    * update notebooks
    
    * transformation notebook: change example to u1 instead of sdf
    
    * ruff
    
    * typo in notebook
    
    * notebook transform: set legend scale
    
    * ruff
    
    * correct typos
    
    * fix typo
    
    * fix plaid type names + reorder some function arguments
    
    * (conda dev env) udpate plaid commit hash
    
    * (notebooks) fix function arguments
    
    * (dependencies) update plaid commit hash
    
    * (CI) fix minor typo
    
    * (doc) minor typo
    
    * (doc) update intersphinx
    
    * (transformations) improve docstrings + uses plaid.utils.stats.OnlineStats to compute bounding box
    
    * (doc) add intersphinx to Muscat
    
    * (feature_engineering) improve docstrings
    
    * (visualization) improve docstrings
    
    * (visualization) add return type hint
    
    * (doc) add intersphinx to PyVista
    
    * update plaid dependency in pyproject.toml
    
    * update environmant.yml
    
    * Minor corrections
    
    ---------
    
    Co-authored-by: Fabien Casenave <fabien.casenave@safrangroup.com>
    Co-authored-by: Xavier Roynard <xavier.roynard@safrangroup.com>

--- src/plaid_ops/__init__.py
+++ src/plaid_ops/__init__.py
@@ -1,3 +1,1 @@
-"""Module that implements PLAID ops."""
-
-print("test")
+"""Package that implement standardized operations for plaid datasets."""
---
+++ tests/conftest.py
@@ -0,0 +1,66 @@
+"""This file defines shared pytest fixtures and test configurations."""
+
+import numpy as np
+import pytest
+from Muscat.Bridges.CGNSBridge import MeshToCGNS
+from Muscat.MeshTools import MeshCreationTools as MCT
+from plaid.containers.dataset import Dataset
+from plaid.containers.sample import Sample
+from plaid.types import CGNSTree
+
+
+@pytest.fixture()
+def nodes():
+    return np.array(
+        [
+            [0.0, 0.0],
+            [1.0, 0.0],
+            [1.0, 1.0],
+            [0.0, 1.0],
+            [0.5, 1.5],
+        ]
+    )
+
+
+@pytest.fixture()
+def triangles():
+    return np.array(
+        [
+            [0, 1, 2],
+            [0, 2, 3],
+            [2, 4, 3],
+        ]
+    )
+
+
+@pytest.fixture()
+def mesh(nodes, triangles):
+    mesh = MCT.CreateMeshOfTriangles(nodes, triangles)
+    mesh.nodeFields["test"] = np.arange(5)
+    return mesh
+
+
+@pytest.fixture()
+def tree(mesh):
+    return MeshToCGNS(mesh)
+
+
+@pytest.fixture()
+def sample():
+    return Sample()
+
+
+@pytest.fixture()
+def sample_with_tree(tree: CGNSTree) -> Sample:
+    """Generate a Sample objects with a tree."""
+    sample = Sample()
+    sample.add_tree(tree)
+    sample.add_scalar("a", 1.0)
+    sample.add_time_series("b", [0.0, 1.0], [3.0, 4.0])
+    return sample
+
+
+@pytest.fixture()
+def dataset(sample_with_tree: Sample) -> Dataset:
+    """Generate a Sample objects with a tree."""
+    return Dataset.from_list_of_samples([sample_with_tree, sample_with_tree])
