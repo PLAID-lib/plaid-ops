ab00d99e1f6c0c045e4b2e0f6ede5d2b8ae0e0ed
Fabien Casenave <fabien.casenave@gmail.com>
2025-09-17 20:17:15 +0200

    update

--- examples/mesh/transformations_example.py
+++ examples/mesh/transformations_example.py
@@ -1,11 +1,12 @@
 # ---
 # jupyter:
 #   jupytext:
+#     custom_cell_magics: kql
 #     text_representation:
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.17.3
+#       jupytext_version: 1.11.2
 #   kernelspec:
 #     display_name: plaid-ops
 #     language: python
